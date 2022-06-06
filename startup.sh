7f4d4e172b5fe1fa2e5c6820db92133b0208fb42
Grégoire Compagnon <obeone@obeone.org>
2022-06-06 09:13:42 +0200

    Adding support for personnal TLS certificate

--- startup.sh
+++ startup.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
+if [ -f /config/private-cert.pem ]; then
+  cp /config/private-cert.pem /home/kasm-user/.vnc/self.pem
+fi
 FREETYPE_PROPERTIES="truetype:interpreter-version=35" wine64 /opt/winbox/winbox64.exe
