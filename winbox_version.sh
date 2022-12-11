091ad514e14d918727a5cc9eecbabddeec3019e4
Grégoire Compagnon <obeone@obeone.org>
2022-12-12 00:24:31 +0100

    Seperation of Winbox download and version finding => optimization

--- winbox_version.sh
+++ winbox_version.sh
@@ -2,5 +2,8 @@
 set -e
 
 WINBOX_URL="$(curl -s -L https://mt.lv/winbox64 -o /dev/null -w '%{url_effective}')"
-mkdir -p /opt/winbox
-wget "${WINBOX_URL}" -O /opt/winbox/winbox64.exe
+if [[ "${WINBOX_URL}" =~ /([0-9.]+)/ ]]; then
+    echo "${BASH_REMATCH[1]}.0"
+else
+    exit 1
+fi
