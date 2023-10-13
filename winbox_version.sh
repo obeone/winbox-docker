31249e182eda550d555f2ec7be8127aed76253dd
Grégoire Compagnon <obeone@obeone.org>
2023-10-13 08:40:45 +0200

    Update winbox_version.sh
    Better version capture

--- winbox_version.sh
+++ winbox_version.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e
 
-WINBOX_URL="$(curl -s -L https://mt.lv/winbox64 -o /dev/null -w '%{url_effective}')"
-if [[ "${WINBOX_URL}" =~ /([0-9.]+)/ ]]; then
-    echo "${BASH_REMATCH[1]}.0"
+WINBOX_URL="$(curl -I -s -L https://mt.lv/winbox64 -o /dev/null -w '%{url_effective}')"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # grep -P doesn't exists on mac
+  version=$(echo $WINBOX_URL | perl -n -e'/\/(\d+\.\d+(?:\.\d+)?)(?=\/[^\/]*$)/ && print $1')
 else
-    exit 1
+  version=$(echo $WINBOX_URL | grep -oP '(?<=/)\d+\.\d+(\.\d+)?(?=/[^/]*$)')
+fi
+
+if [ -z "$version" ]; then
+  echo "No version number found !"
+  exit 1
+else
+  echo ${version}.0
 fi
